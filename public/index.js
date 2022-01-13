f92adfb40eb8b8faccde82dc52e6566717a75929
Qwazzy1990 <disalvopatrick@gmail.com>
2022-01-13 15:54:55 -0500

    Work on list page

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+//labels and inputs
 var nameLabel =  `<label for=\"name\"> Item Name: </label>`;
 var nameField = `<input type="text" id="name" name="name" />`;
 var manufacturerLabel =  `<label for=\"manufacturer\"> Manufacturer Name: </label>`;
@@ -6,7 +7,19 @@ var serialNoLabel =  `<label for=\"serialNo\"> Serial Number: </label>`;
 var serialNoInput = `<input type="text" id="serialNo" name="serialNo" />`;
 var quantityLabel = `<label for=\"quantity\"> Quantity:</label>`;
 var quantityInput = `<input type="text" id="quantityNo" name="quantityNo" />`;
-var submitButton = `<button type="button" id="btn-logout" class="btn btn-outline">Submit</button>`;
+
+//buttons
+var addItemButton = `<button type="button" id="submit-add" class="btn btn-dark">Add Item</button>`;
+var editItemButton = `<button type="button" id="submit-edit" class="btn btn-dark">Edit Item</button>`;
+var deleteItemButton = `<button type="button" id="submit-delete" class="btn btn-dark">Delete Item</button>`;
+
+
+//labels for scrollable box
+var serialNosLabel = `<label> Serial Numbers: </label><br>`;
+var serialNoBoard = '<div style="height:120px;width:300px;border:1px solid #ccc;overflow-y:auto;" id="serialNoBoard"></div>';
+
+var deleteItemsLabel = `<label> Serial Numbers/Amounts: </label><br>`;
+
 
 
 
@@ -15,21 +28,41 @@ $(document).ready(function () {
 	
 	//load uploadpage.html
 	$('#btn-add').click(() => {
-		alert("here");
 	
 		$('#main-div').empty();
-		$('#main-div').append(getAddPage());
+		$('#main-div').append(getMainPage());
+		$('#add-container').append(addItemButton);
 		//$('.btn-navbar').attr('disabled', false);
 		//$('#btn-add').attr('disabled', true);
 		
 	});
 
-	$('#btn-search').click(()=>{
+	$('#btn-edit').click(()=>{
 		//send search page
+		$('#main-div').empty();
+		$('#main-div').append(getMainPage());
+		$(`#add-container`).append(editItemButton);
+		$('#main-div').append(serialNosLabel);
+		$('#main-div').append(serialNoBoard);
+		$('#serialNoBoard').append(makeSerialNoList([1111, 222, 333, 444, 55, 555, 6666, 666, 6655]));
+
+
 	});
 
 	$('#btn-delete').click(()=>{
 		//send delete page
+		//send search page
+		$('#main-div').empty();
+		$('#main-div').append(getDeletePage());
+		$('#add-container').append(deleteItemButton);
+		$('#main-div').append(deleteItemsLabel);
+		$('#main-div').append(serialNoBoard);
+		$('#serialNoBoard').append(makeDeleteList([{sNo:123, amnt:22}, {sNo:22222, amnt:3333}, {sNo:12345, amnt:666}, {sNo:444, amnt:123}, {sNo:55, amnt:222}]));
+
+
+
+
+
 	});
 
 
@@ -52,7 +85,7 @@ $(document).ready(function () {
 
 
 
-function getAddPage()
+function getMainPage()
 {
 	var s =  `<div class=\"jumbotron jumbotron-fluid\" id=\"homepage-jumbotron\">`
 	 + `<div class=\"container center\" id=\"add-container\">`
@@ -65,10 +98,53 @@ function getAddPage()
 	 +`<br><br>`
 	 +`${quantityLabel} ${quantityInput}`
 	 +'<br><br>'
-	 + `${submitButton}`
 	 + "</div>"
 	 + "</div>";
 
 	 return s;
 
+}
+
+function makeSerialNoList(data)
+{
+	var s = `<ul>`;
+	for(var i = 0; i < data.length; i++)
+	{
+		s += `<li>`
+		s += String(data[i]);
+		s += `</li>`;
+	}
+	s += `</ul>`;
+	return s;
+}
+
+function getDeletePage()
+{
+	var s =  `<div class=\"jumbotron jumbotron-fluid\" id=\"homepage-jumbotron\">`
+	 + `<div class=\"container center\" id=\"add-container\">`
+	 +`<br><br><br>`
+	 +`${serialNoLabel} ${serialNoInput}`
+	 +`<br><br>`
+	 +`${quantityLabel} ${quantityInput}`
+	 +'<br><br>'
+	 + "</div>"
+	 + "</div>";
+
+	 return s;
+
+}
+
+function makeDeleteList(data)
+{
+	var s = `<ul>`;
+	for(var i = 0; i < data.length; i++)
+	{
+		s += `<li>`
+		s += String(data[i].sNo);
+		s += "/"
+		s += String(data[i].amnt);
+		s += `</li>`;
+	}
+	s += `</ul>`;
+	return s;
 }
